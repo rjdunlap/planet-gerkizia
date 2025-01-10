5e3d7c1dd7941580273453b56e903262566e20b6
rjdunlap <rjdunlap@outlook.com>
2025-01-10 12:56:18 -0800

    First build

--- data.lua
+++ data.lua
@@ -1,16 +1,25 @@
 --data.lua
+local asteroid_util = require("__space-age__.prototypes.planet.asteroid-spawn-definitions")
+
 
 --START MAP GEN
 function MapGen_Gerkizia()
     -- Nauvis-based generation
     local map_gen_setting = table.deepcopy(data.raw.planet.nauvis.map_gen_settings)
-    map_gen_setting.water = "very high"
-    --map_gen_setting.property_expression_names.elevation = "gerkizia"
+    --map_gen_setting.property_expression_names.elevation = "lakes"
 
-    map_gen_setting.autoplace_settings.entity.settings = {
-        ["uranium-ore"] = {}
+    --map_gen_setting.terrain_segmentation = "very-high"
+
+    map_gen_setting.autoplace_controls = {
+        ["stone"] = { frequency = 9, size = 9, richness = 12 },
+        ["iron-ore"] = { frequency = 6, size = 6, richness = 6 },
+        ["coal"] = { frequency = 6, size = 6, richness = 6 },
+        ["copper-ore"] = { frequency = 6, size = 6, richness = 6 },
+        ["crude-oil"] = { frequency = 6, size = 6, richness = 6 },
+        ["trees"] = { frequency = 6, size = 6, richness = 6 },
+        ["water"] = { frequency = 12, size = 6, richness = 6 },
+        ["uranium-ore"] = { frequency = 0, size = 0, richness = 0 },
     }
-
     return map_gen_setting
 end
 -- increse stone patch size in start area
@@ -41,18 +50,58 @@ local gerkizia=
     map_gen_settings = MapGen_Gerkizia()
 }
 
+gerkizia.orbit = {
+    parent = {
+        type = "space-location",
+        name = "star",
+    },
+    distance = 1,
+    orientation = 0.78
+}
+
 local gerkizia_connection = {
     type = "space-connection",
     name = "nauvis-gerkizia",
     from = "nauvis",
     to = "gerkizia",
     subgroup = data.raw["space-connection"]["nauvis-vulcanus"].subgroup,
-    length = 15000
-  
+    length = 15000,
+    asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.nauvis_gleba),
   }
 
 PlanetsLib:extend({gerkizia})
 
 data:extend{gerkizia_connection}
 
-APS.add_planet{name = "gerkizia", filename = "__planet-gerkizia__/data.lua"}
+data:extend {{
+    type = "technology",
+    name = "planet-discovery-gerkizia",
+    icons = util.technology_icon_constant_planet("__planet-gerkizia__/graphics/planet-gerkizia.png"),
+    icon_size = 256,
+    essential = true,
+    localised_description = {"space-location-description.gerkizia"},
+    effects = {
+        {
+            type = "unlock-space-location",
+            space_location = "gerkizia",
+            use_icon_overlay_constant = true
+        },
+    },
+    prerequisites = {
+        "space-science-pack",
+    },
+    unit = {
+        count = 200,
+        ingredients = {
+            {"automation-science-pack",      1},
+            {"logistic-science-pack",        1},
+            {"chemical-science-pack",        1},
+            {"space-science-pack",           1}
+        },
+        time = 60,
+    },
+    order = "ea[gerkizia]",
+}}
+
+
+APS.add_planet{name = "gerkizia", filename = "__planet-gerkizia__/gerkizia.lua", technology = "planet-discovery-gerkizia"}

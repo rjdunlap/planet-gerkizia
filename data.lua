76a1dcfd85427ee9d1a530aa3b2767d00c8fe7bf
rjdunlap <rjdunlap@outlook.com>
2025-01-15 18:08:38 -0800

    fix

--- data.lua
+++ data.lua
@@ -63,7 +63,7 @@ local gerkizia=
     },
     map_gen_settings = MapGen_Gerkizia(),
     asteroid_spawn_influence = 1,
-    asteroid_spawn_definitions = gerkizia_astroid_spawn
+    asteroid_spawn_definitions = start_astroid_spawn
     
 }
 
